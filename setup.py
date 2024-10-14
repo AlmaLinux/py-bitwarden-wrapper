ac144c58a883f1a7c5d05444a0b4d978c5366f40
Vasily Kleschov <kleshev12@gmail.com>
2024-10-14 13:39:55 +0200

    Bump version to 0.1.15

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="bitwarden-wrapper",
-    version="0.1.14",
+    version="0.1.15",
     author="Vasily Kleschov",
     author_email="kleshev12@gmail.com",
     description="The python wrapper around Bitwarden CLI.",
